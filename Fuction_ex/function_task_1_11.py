5a83adff9d0a6e83198f7bd52f2db8dcffe0f7dd
dmoho <dfarfek05@gmail.com>
2021-04-25 16:03:05 +0300

    git

---
+++ Fuction_ex/function_task_1_11.py
@@ -0,0 +1,7 @@
+def fill_list(l1):
+    for i in range(2,41,2):
+        l1.append(i)
+    return l1
+
+list_1=[]
+print(fill_list(list_1))
---
+++ Fuction_ex/function_task_1_12.py
@@ -0,0 +1,15 @@
+import random
+def fill_list_2(l1):
+    for i in range(20):
+        ran_num=random.randint(1,100)
+        l1.append(ran_num)
+    return l1
+def list_repeats(list_1,num_1):
+    for i in range(num_1):
+        print(list_1)
+
+list_1=[]
+print(fill_list_2(list_1))
+user_num=int(input("Enter quantity of repeats: "))
+list_repeats(list_1,user_num)
+
