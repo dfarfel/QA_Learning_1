857c77892f1998077bf1d2f99e3a7d7e77fb9690
dmoho <dfarfek05@gmail.com>
2021-04-26 15:33:08 +0300

    git

---
+++ Class_object/Class_task_5.py
@@ -0,0 +1,28 @@
+class HardDisk:
+    def __init__(self):
+        self.capacity = 100
+        self.us_space = 0
+        self.num_file = 0
+
+    def print_(self):
+        print(f'Capacity on your disk - {self.capacity}\nUsed space is - {self.us_space}\nYou have {self.num_file} files on your disk')
+
+    def freeSpace(self):
+        free=self.capacity-self.us_space
+        return free
+
+    def addFile(self,GB_size):
+        if GB_size<self.freeSpace():
+            self.us_space+=GB_size
+            self.num_file += 1
+            return print("Done")
+        else:
+            print("You don't have enough space on you disk")
+    def delFile(self,GB_size):
+        if self.freeSpace()+GB_size >= self.capacity:
+            self.us_space=0
+            self.freeSpace()
+            self.num_file-=1
+        else:
+            self.us_space-=GB_size
+            self.num_file-=1
---
+++ Class_object/Class_task_5_main.py
@@ -0,0 +1,10 @@
+from Class_object.Class_task_5 import HardDisk
+
+hard_disk_1 = HardDisk()
+
+for i in range(5):
+    hard_disk_1.addFile(21)
+
+hard_disk_1.delFile(101)
+
+hard_disk_1.print_()
